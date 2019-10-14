1638ca44e0ff21d39bac21ad239578eca58bef0f
WangJunZzz <510423039@qq.com>
2019-10-14 18:08:57 +0800

    修改Product

--- Fairhr.Cap/Fairhr.Service.B/LogicHandler/ProductLogicHandler.cs
+++ Fairhr.Cap/Fairhr.Service.B/LogicHandler/ProductLogicHandler.cs
@@ -9,12 +9,27 @@ namespace Fairhr.Service.B.LogicHandler
 {
     public class ProductLogicHandler : IProductLogicHandler, ICapSubscribe
     {
+        private readonly WXPayInfoContext db;
+        public ProductLogicHandler(WXPayInfoContext context)
+        {
+            db = context;
+        }
+
         [CapSubscribe("send.product")]
         public object ReceiveProductInfo(object obj)
         {
+            PayInfo payInfo = new PayInfo();
+            payInfo.Id = Guid.NewGuid().ToString("N");
+            payInfo.Money = "100";
+            payInfo.OrderId += 1;
+            payInfo.stauts = 1;
+            payInfo.CrateTime = DateTime.Now;
+            payInfo.UpdateTime = DateTime.Now;
+            db.PayInfo.Add(payInfo);
+            db.SaveChanges();
             // 具体业务
-
-            return obj;
+            throw new NullReferenceException("空指针异常.");
+            //return obj;
         }
 
         public void RollBack()
